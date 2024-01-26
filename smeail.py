84474ee61d924df0c152a9e61304bea2af902335
tafforzore <nzomutchawilfrid@gmail.com>
2024-01-26 10:51:08 +0100

    sss

--- smeail.py
+++ smeail.py
@@ -3,37 +3,41 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 # Paramètres du serveur SMTP
-smtp_server = 'tandisgicaedef.cm'
-smtp_port = 465  # Utilisez le port approprié pour votre configuration (peut être 465 pour SSL/TLS)
-smtp_username = 'tandis@tandisgicaedef.cm'
-smtp_password = '*Severin123'
+smtp_server = "mail.tandisgicaedef.cm"
+smtp_port = 587  # Le port SMTP peut varier selon la configuration du serveur
 
-# Paramètres de l'email
-sender_email = 'tandis@tandisgicaedef.cm'
-recipient_email = 'testpad24@gmail.com'
-subject = 'Test SMTP'
-body = 'Ceci est un test d\'envoi d\'email depuis Python.'
+# Informations d'authentification
+smtp_username = "tandis@tandisgicaedef.cm"
+smtp_password = "*Severin123"
 
-# Création de l'email
-message = MIMEMultipart()
-message['From'] = sender_email
-message['To'] = recipient_email
-message['Subject'] = subject
-message.attach(MIMEText(body, 'plain'))
-server = smtplib.SMTP(smtp_server, smtp_port)
+# Destinataire et expéditeur
+from_email = "tandis@tandisgicaedef.cm"
+to_email = "nzomutchawilfrid@gmail.com"
+
+# Objet du message
+subject = "Objet de votre e-mail"
+
+# Corps du message
+body = "Corps de votre e-mail."
+
+# Création de l'objet MIMEMultipart
+msg = MIMEMultipart()
+msg['From'] = from_email
+msg['To'] = to_email
+msg['Subject'] = subject
+
+# Ajout du corps du message
+msg.attach(MIMEText(body, 'plain'))
 
 # Connexion au serveur SMTP
-try:
-    server.starttls()  # Utilisez starttls() pour le mode STARTTLS, ou retirez cette ligne pour SSL/TLS
-    server.login(smtp_username, smtp_password)
+server = smtplib.SMTP(smtp_server, smtp_port)
+server.starttls()  # Utilisation de TLS, peut varier selon la configuration du serveur
 
-    # Envoi de l'email
-    server.sendmail(sender_email, recipient_email, message.as_string())
-    print('Email envoyé avec succès!')
+# Authentification
+server.login(smtp_username, smtp_password)
 
-except Exception as e:
-    print(f'Erreur lors de l\'envoi de l\'email : {e}')
+# Envoi de l'e-mail
+server.sendmail(from_email, to_email, msg.as_string())
 
-finally:
-    # Fermeture de la connexion au serveur SMTP
-    server.quit()
+# Fermeture de la connexion
+server.quit()
